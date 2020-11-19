2b3699ca5f628f597aab4d5ab2e47e83685e83af
Shikhar Dixit <shikhar.dixit@stackroute.in>
2020-11-19 12:47:56 +0530

    starter boilerplate code

--- src/logger.config.ts
+++ src/logger.config.ts
@@ -2,7 +2,7 @@ const log4js = require('log4js');
 
 // specifying the configuration of the log4js, the name and the type.
 log4js.configure({
-  appenders: { EagleEyeETLConnector: { type: 'console' } },
+  appenders: { BoilerPlateCode: { type: 'console' } },
   categories: { default: { appenders: ['BoilerPlateCode'], level: 'debug' } }
 });
 const logger = log4js.getLogger();
