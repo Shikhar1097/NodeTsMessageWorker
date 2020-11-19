c79ed5319291e94e6aab7f081b2dd9c0cc6be1ba
Shikhar Dixit <shikhar.dixit@stackroute.in>
2020-11-19 15:17:06 +0530

    updated rabbitmq uri

--- src/appConfigs/env/local.ts
+++ src/appConfigs/env/local.ts
@@ -1,6 +1,6 @@
 const config = {
   AMQP: {
-    AMQP_URL: 'amqp://EagleEye:eag!eEyE@localhost',
+    AMQP_URL: 'amqp://username:password@localhost',
     AMQP_QUEUE: 'Boilerplate-Queue',
     AMQP_EXCHANGE: 'Boilerplate-Exchange'
   }
